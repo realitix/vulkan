0878987cc6dea98c46d639c2c28ad18473ba280d
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-10-02 17:23:48 +0200

    Update to v1.0.61-core

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='vulkan',
-    version='1.0.49',
+    version='1.0.61',
     description='Ultimate Python binding for Vulkan API',
     author='realitix',
     author_email='realitix@gmail.com',
